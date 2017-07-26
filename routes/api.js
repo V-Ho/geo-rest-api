a86d890c3a772b2ec812940d445c5e072ebdd2f1
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-07-26 15:42:02 +0200

    Add updated PUT request routes

--- routes/api.js
+++ routes/api.js
@@ -17,7 +17,11 @@ router.post('/ninjas',function(req,res,next){
 
 //update a ninja in db
 router.put('/ninjas/:id',function(req,res,next){
-  res.send({type:'PUT'});
+  Ninja.findByIdAndUpdate({_id:req.params.id},req.body).then(function(){
+    Ninja.findOne({_id:req.params.id}).then(function(ninja){
+      res.send(ninja);
+    });
+  });
 });
 
 //delete a ninja from db, need params(id)

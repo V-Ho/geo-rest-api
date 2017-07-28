9bf51d31329b1932e73a8c9aabbef910adfc4e0b
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-07-28 13:11:20 +0200

    create front end

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser'); //remember to include above middlewar
 const mongoose = require('mongoose');
 const app = express();
 
-app.use(bodyParser.json());//we want bodyParser middleware to parse json before it reaches route handler
-
 //connect to mongodb - mongose provides connect method to find db connection stream
 mongoose.connect('mongodb://localhost/ninjago');
 mongoose.Promise = global.Promise; //setting mongoose to global promise bc Mongodb's version is deprecated
 
+app.use(express.static('public')); //express serves static files in Public folder
+
+app.use(bodyParser.json());//we want bodyParser middleware to parse json before it reaches route handler
 
 //initialise routes to all use /api route, and require the routes exported from api.js
 app.use('/api',require('./routes/api'));
